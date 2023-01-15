0fba6a9e554b6f20184469832bc7fb6bdde28577
danil.sotnick <danil.sotnick@yandex.ru>
2023-01-15 09:57:13 +0700

    Исправлены замечания, написан стресс тест

---
+++ src/StressTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.Devices;
+using KompasWrapper;
+using Core;
+
+namespace StressTest
+{
+    class Program
+    {
+        private static void Main()
+        {
+            var stopWatch = new Stopwatch();
+            var connector = new KompasConnector();
+            stopWatch.Start();
+            var swordParameters = new SwordParameters();
+            var swordBuilder = new SwordBuilder();
+            var streamWriter = new StreamWriter($"StressTest.txt", true);
+            var modelCounter = 0;
+            var computerInfo = new ComputerInfo();
+            ulong usedMemory = 0;
+            while (usedMemory * 0.96 <= computerInfo.TotalPhysicalMemory)
+            {
+                swordBuilder.BuildSword(swordParameters);
+                usedMemory = (computerInfo.TotalPhysicalMemory - computerInfo.AvailablePhysicalMemory);
+                streamWriter.WriteLine(
+                    $"{++modelCounter}\t{stopWatch.Elapsed:hh\\:mm\\:ss}\t{usedMemory}");
+                streamWriter.Flush();
+            }
+            stopWatch.Stop();
+            streamWriter.WriteLine("END");
+            streamWriter.Close();
+            streamWriter.Dispose();
+        }
+    }
+}
